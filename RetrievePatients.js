8c60dba7586d861c238966a1a3113ce12e9729f8
LeeJD1992 <lee.donald@hotmail.co.uk>
2023-05-24 13:40:50 +0100

    update get patients page

--- RetrievePatients.js
+++ RetrievePatients.js
@@ -1,4 +1,6 @@
-fetch('/RetrievePatients')
+const fetch = require('node-fetch');
+
+fetch('http://localhost:3000/RetrievePatients')
   .then(response => {
     if (!response.ok) {
       throw new Error('Network response was not OK');
@@ -6,31 +8,8 @@ fetch('/RetrievePatients')
     return response.json();
   })
   .then(data => {
-    const table = document.createElement('table');
-    const headerRow = document.createElement('tr');
-    const columns = Object.keys(data[0]);
-
-    columns.forEach(column => {
-      const th = document.createElement('th');
-      th.textContent = column;
-      headerRow.appendChild(th);
-    });
-
-    table.appendChild(headerRow);
-
-    data.forEach(rowData => {
-      const row = document.createElement('tr');
-
-      columns.forEach(column => {
-        const td = document.createElement('td');
-        td.textContent = rowData[column];
-        row.appendChild(td);
-      });
-
-      table.appendChild(row);
-    });
-
-    document.body.appendChild(table);
+    // Process the received data as needed
+    console.log(data);
   })
   .catch(error => {
     console.error('Error:', error);
